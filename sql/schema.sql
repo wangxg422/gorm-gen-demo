ef30b5bcb1819ff2910234b3be4b7583e9eabcb1
wangxg <wangxg422@outlook.com>
2025-09-19 02:31:54 +0000

    change sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,105 +1,42 @@
-/*
- Navicat Premium Dump SQL
-
- Source Server         : mysql-dev-fnos
- Source Server Type    : MySQL
- Source Server Version : 80030 (8.0.30)
- Source Host           : 10.66.66.66:13307
- Source Schema         : gorm-gen-demo
-
- Target Server Type    : MySQL
- Target Server Version : 80030 (8.0.30)
- File Encoding         : 65001
-
- Date: 16/05/2025 00:02:02
-*/
-
-SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for app_instance
--- ----------------------------
-DROP TABLE IF EXISTS `app_instance`;
-CREATE TABLE `app_instance` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `app_id` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0' COMMENT 'app实例id',
-  `app_package_id` bigint NOT NULL COMMENT '安装包id',
-  `app_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'app实例名称',
-  `create_user_id` bigint NOT NULL COMMENT '创建用户',
-  `desc` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'app描述',
-  `status` tinyint NOT NULL DEFAULT '0' COMMENT '0正常1停止',
-  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0' COMMENT '0可用1已删除',
-  `create_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  `delete_time` datetime DEFAULT NULL,
-  `remark` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Table structure for app_package
--- ----------------------------
-DROP TABLE IF EXISTS `app_package`;
-CREATE TABLE `app_package` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `package_id` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '应用包id',
-  `package_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '应用包名称',
-  `package_label` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '应用包标签',
-  `package_version` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '应用包版本',
-  `create_user_id` bigint NOT NULL COMMENT '创建用户',
-  `status` tinyint NOT NULL DEFAULT '0' COMMENT '0可用1禁用',
-  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0' COMMENT '0可用1已删除',
-  `create_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  `delete_time` datetime DEFAULT NULL,
-  `remark` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Table structure for role
--- ----------------------------
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `role_code` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色编码',
-  `role_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色名称',
-  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0' COMMENT '0可用1已删除',
-  `status` tinyint NOT NULL DEFAULT '0' COMMENT '0可用1停用',
-  `create_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  `delete_time` datetime DEFAULT NULL,
-  `remark` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
-  `real_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '姓名',
-  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
-  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0' COMMENT '0可用1已删除',
-  `status` tinyint NOT NULL DEFAULT '0' COMMENT '0正常1停用',
-  `create_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  `delete_time` datetime DEFAULT NULL,
-  `remark` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Table structure for user_role
--- ----------------------------
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role` (
-  `user_id` bigint NOT NULL,
-  `role_id` bigint NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+SET NAMES utf8mb4;
+-- sys_dept DDL
+CREATE TABLE `sys_dept` (`id` BIGINT NOT NULL AUTO_INCREMENT Comment "id",
+`parent_id` BIGINT NULL,
+`dept_name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+`create_time` DATETIME NULL,
+`update_time` DATETIME NULL,
+`delete_time` DATETIME NULL,
+`del_flag` TINYINT NULL,
+`remark` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+PRIMARY KEY (`id`)) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+-- sys_role DDL
+CREATE TABLE `sys_role` (`id` BIGINT NOT NULL AUTO_INCREMENT,
+`role_code` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL Comment "角色编码",
+`role_name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL Comment "角色名称",
+`del_flag` CHAR(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0' Comment "0可用1已删除",
+`status` TINYINT NOT NULL DEFAULT 0 Comment "0可用1停用",
+`create_time` DATETIME NULL,
+`update_time` DATETIME NULL,
+`delete_time` DATETIME NULL,
+`remark` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+PRIMARY KEY (`id`)) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci AUTO_INCREMENT = 2 ROW_FORMAT = Dynamic;
+-- sys_user DDL
+CREATE TABLE `sys_user` (`id` BIGINT NOT NULL AUTO_INCREMENT,
+`dept_id` BIGINT NULL,
+`user_name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL Comment "用户名",
+`real_name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL Comment "姓名",
+`password` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL Comment "密码",
+`del_flag` CHAR(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0' Comment "0可用1已删除",
+`status` TINYINT NOT NULL DEFAULT 0 Comment "0正常1停用",
+`create_time` DATETIME NULL,
+`update_time` DATETIME NULL,
+`delete_time` DATETIME NULL,
+`remark` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+PRIMARY KEY (`id`)) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci AUTO_INCREMENT = 18 ROW_FORMAT = Dynamic;
+-- sys_user_role DDL
+CREATE TABLE `sys_user_role` (`user_id` BIGINT NOT NULL,
+`role_id` BIGINT NOT NULL,
+PRIMARY KEY (`user_id`,
+`role_id`)) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 SET FOREIGN_KEY_CHECKS = 1;
